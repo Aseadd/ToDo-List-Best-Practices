74d0de40c9842a52207220b60289325ede98f518
addis <betsega23@gmail.com>
2022-08-15 23:39:46 +0300

    big chunk

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import './style.css';
+import Tasks from './task.js';
+import showAllTodo from './function.js';
+// import slist from './drag.js';
+
+const todos = new Tasks();
+
+const form = document.querySelector('.add-list');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const items = {
+    description: form.elements.todo.value,
+  };
+  form.reset();
+  todos.addTodo(items);
+  showAllTodo(todos);
+  // slist(todos);
+});
+
+showAllTodo(todos);
+
+const clear = document.querySelector('.remove');
+
+clear.addEventListener('click', () => {
+  todos.clearAll();
+  showAllTodo(todos);
+});
